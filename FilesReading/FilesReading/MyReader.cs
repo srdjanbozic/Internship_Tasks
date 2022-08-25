96d233a697dffe022c55b5c30589fdb5ec192880
Srdjan Bozic <57723396+srdjanbozic@users.noreply.github.com>
2022-08-26 00:56:11 +0200

    Add files via upload

---
+++ FilesReading/FilesReading/MyReader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilesReading
+{
+    internal class MyReader
+    {
+        private readonly StreamReader _streamReader;
+        public MyReader(StreamReader sr)
+        {
+            _streamReader = sr;
+        }
+    }
+}
---
+++ FilesReading/FilesReading/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        try
+        {
+            using (StreamReader reader = new StreamReader("StringReader.txt"))
+            {
+                Console.WriteLine("Print content of .txt files: ");
+                Console.WriteLine(); 
+                Console.WriteLine(reader.ReadToEnd());
+            }
+        } catch(Exception e)
+        {
+                Console.WriteLine("The file could not be read: ");
+                Console.WriteLine(e.ToString());
+        }
+    }
+}
